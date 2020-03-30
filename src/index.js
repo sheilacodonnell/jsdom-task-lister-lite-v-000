37aefe817de4930b6a7760217d7dd9d940f0ae6d
Sheila O'Donnell <sheila@contexttravel.com>
2020-03-30 19:00:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   form.addEventListener('submit', function(event) {
     event.preventDefault();
-      let listElement = document.createElement("li");
-
+    let listElement = document.createElement("li");
     listElement.innerHTML = task.value;
     taskList.appendChild(listElement);
   });
