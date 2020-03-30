12dfcab9e7da34a9b7dac32d72870cf365bf668e
Sheila O'Donnell <sheila@contexttravel.com>
2020-03-30 18:36:39 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const task = document.getElementById("new-task-description");
   const submit = document.getElementById("submit");
   const list = document.getElementById("list");
+  const form = document.getElementById("form");
+
   form.addEventListener('submit', function(event) {
     event.preventDefault()
   listItem.appendChild(task.value);
